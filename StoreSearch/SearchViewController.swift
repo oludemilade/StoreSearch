5bca6996b8b7b5e5b3b044aa239f868051425233
Oludemilade raji <rajiolu@gmail.com>
2018-06-09 16:27:21 -0700

    No result when app first loads

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -14,6 +14,7 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var searchResults = [SearchResult]()
+    var hasSearched = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,13 +45,17 @@ extension SearchViewController: UISearchBarDelegate{
                 searchResults.append(searchResult)
             }
         }
+        hasSearched = true
         tableView.reloadData()
     }
 }
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if searchResults.count == 0 {
+        if !hasSearched {
+            return 0 
+        }
+        else if searchResults.count == 0 {
             return 1
         } else {
             return searchResults.count
