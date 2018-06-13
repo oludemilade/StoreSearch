bb18779464fb89e5edcc15180af6172464e8ea5f
Oludemilade raji <rajiolu@gmail.com>
2018-06-12 22:52:56 -0700

    Add the ability to search for different types of store items.

--- Model/SearchResult.swift
+++ Model/SearchResult.swift
@@ -15,28 +15,57 @@ class ResultArray:Codable {
 
 class SearchResult:Codable, CustomStringConvertible {
     var description: String{
-        return "Kind: \(kind), Name: \(name), Artist Name: \(artistName)"
+        return "Kind: \(kind ?? ""), Name: \(name), Artist Name: \(artistName)"
     }
     
-    var trackPrice = 0.0
+    var trackPrice : Double?
     var currency = ""
     var imageSmall = ""
     var imageLarge = ""
-    var storeURL = ""
-    var genre = ""
-    var trackName = ""
+    var trackName : String?
     var artistName = ""
-    var kind = ""
+    var kind : String?
+    var trackViewUrl : String?
+    var collectionName : String?
+    var collectionViewUrl : String?
+    var collectionPrice : Double?
+    var itemGenre: String?
+    var itemPrice : Double?
+    var bookGenre: [String]?
     
     enum CodingKeys: String, CodingKey{
         case imageSmall = "artworkUrl60"
         case imageLarge = "artworkUrl100"
-        case storeURL = "trackViewUrl"
-        case genre = "primaryGenreName"
-        case kind, artistName, trackName
-        case trackPrice, currency
+        case itemGenre = "primaryGenreName"
+        case bookGenre = "genres"
+        case itemPrice = "price"
+        case kind, artistName, currency
+        case trackName, trackPrice, trackViewUrl
+        case collectionName, collectionViewUrl, collectionPrice
     }
+    
     var name: String{
-        return trackName
+        return trackName ?? collectionName ?? ""
+    }
+    
+    var storeURL : String {
+        return trackViewUrl ?? collectionViewUrl ?? ""
+    }
+    
+    var price : Double {
+        return trackPrice ?? collectionPrice ?? itemPrice ?? 0.0
+    }
+    
+    var genre : String {
+        if let genre = itemGenre {
+            return genre
+        } else if let genres = bookGenre {
+            return genres.joined(separator: ", ")
+        }
+        return ""
+    }
+    
+    var type : String {
+        return kind ?? "audiobook"
     }
 }
